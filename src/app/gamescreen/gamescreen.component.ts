2f52f96537d96acc0a660b6e7a30b80af6f01acd
Travis Mix <travismix1980@gmail.com>
2021-02-15 18:06:15 -0800

    Random latin sentence is being generated and displayed in proper location

--- src/app/gamescreen/gamescreen.component.ts
+++ src/app/gamescreen/gamescreen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-gamescreen',
@@ -10,6 +11,10 @@ export class GamescreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  generateNewSentence(): string {
+    return lorem.sentence();
   }
 
 }
