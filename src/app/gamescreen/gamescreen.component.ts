05940b10f1060d92b1d3c71091e5edd19f28256a
Travis Mix <travismix1980@gmail.com>
2021-02-15 18:30:05 -0800

    getting and storing user input

--- src/app/gamescreen/gamescreen.component.ts
+++ src/app/gamescreen/gamescreen.component.ts
@@ -4,17 +4,22 @@ import { lorem } from 'faker';
 @Component({
   selector: 'app-gamescreen',
   templateUrl: './gamescreen.component.html',
-  styleUrls: ['./gamescreen.component.css']
+  styleUrls: ['./gamescreen.component.css'],
 })
 export class GamescreenComponent implements OnInit {
+  randomText = lorem.sentence();
+  inputValue: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onInput(value: string): void {
+    this.inputValue = value;
+    // DEBUG: console.log(this.inputValue);
   }
 
-  generateNewSentence(): string {
-    return lorem.sentence();
-  }
-
+  // generateNewSentence(): string {
+  //   return lorem.sentence();
+  // }
 }
